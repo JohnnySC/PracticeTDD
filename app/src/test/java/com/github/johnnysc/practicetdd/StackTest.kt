232a6debac3290fd660a147a48414e38887d1856
hovhannes <johnnysc091@gmail.com>
2022-12-25 18:57:04 +0900

    FIFO cases added

--- app/src/test/java/com/github/johnnysc/practicetdd/StackTest.kt
+++ app/src/test/java/com/github/johnnysc/practicetdd/StackTest.kt
@@ -10,23 +10,51 @@ class StackTest {
 
     @Test(expected = IllegalStateException::class)
     fun `test invalid object negative count`() {
-        MyStack.Base<CustomObject>(maxCount = -1)
+        MyStack.LIFO<CustomObject>(maxCount = -1)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `test invalid object negative count fifo`() {
+        MyStack.FIFO<CustomObject>(maxCount = -1)
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test invalid object zero count`() {
-        MyStack.Base<CustomObject>(maxCount = 0)
+        MyStack.LIFO<CustomObject>(maxCount = 0)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `test invalid object zero count fifo`() {
+        MyStack.FIFO<CustomObject>(maxCount = 0)
     }
 
     @Test(expected = IllegalStateException::class)
     fun `test pop item from empty stack`() {
-        val stack = MyStack.Base<CustomObject>(maxCount = 1)
+        val stack = MyStack.LIFO<CustomObject>(maxCount = 1)
+        stack.pop()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `test pop item from empty stack fifo`() {
+        val stack = MyStack.FIFO<CustomObject>(maxCount = 1)
         stack.pop()
     }
 
     @Test
     fun `test push more items than max count`() {
-        val stack = MyStack.Base<CustomObject>(maxCount = 1)
+        val stack = MyStack.LIFO<CustomObject>(maxCount = 1)
+        stack.push(item = CustomObject("1"))
+        try {
+            stack.push(item = CustomObject("2"))
+        } catch (e: Exception) {
+            assertEquals(true, e is IllegalStateException)
+            assertEquals("Stack overflow exception, maximum is 1", e.message)
+        }
+    }
+
+    @Test
+    fun `test push more items than max count fifo`() {
+        val stack = MyStack.FIFO<CustomObject>(maxCount = 1)
         stack.push(item = CustomObject("1"))
         try {
             stack.push(item = CustomObject("2"))
@@ -38,7 +66,15 @@ class StackTest {
 
     @Test(expected = IllegalStateException::class)
     fun `test pop more than pushed`() {
-        val stack = MyStack.Base<CustomObject>(maxCount = 1)
+        val stack = MyStack.LIFO<CustomObject>(maxCount = 1)
+        stack.push(item = CustomObject("1"))
+        stack.pop()
+        stack.pop()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `test pop more than pushed fifo`() {
+        val stack = MyStack.FIFO<CustomObject>(maxCount = 1)
         stack.push(item = CustomObject("1"))
         stack.pop()
         stack.pop()
@@ -46,7 +82,16 @@ class StackTest {
 
     @Test
     fun `test pop`() {
-        val stack = MyStack.Base<CustomObject>(maxCount = 1)
+        val stack = MyStack.LIFO<CustomObject>(maxCount = 1)
+        stack.push(item = CustomObject("1"))
+        val actual = stack.pop()
+        val expected = CustomObject("1")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test pop fifo`() {
+        val stack = MyStack.FIFO<CustomObject>(maxCount = 1)
         stack.push(item = CustomObject("1"))
         val actual = stack.pop()
         val expected = CustomObject("1")
@@ -55,7 +100,7 @@ class StackTest {
 
     @Test
     fun `test pop twice`() {
-        val stack = MyStack.Base<CustomObject>(maxCount = 2)
+        val stack = MyStack.LIFO<CustomObject>(maxCount = 2)
         stack.push(item = CustomObject("1"))
         stack.push(item = CustomObject("2"))
         var actual = stack.pop()
@@ -67,8 +112,34 @@ class StackTest {
     }
 
     @Test
+    fun `test pop twice fifo`() {
+        val stack = MyStack.FIFO<CustomObject>(maxCount = 2)
+        stack.push(item = CustomObject("1"))
+        stack.push(item = CustomObject("2"))
+        var actual = stack.pop()
+        var expected = CustomObject("1")
+        assertEquals(expected, actual)
+        actual = stack.pop()
+        expected = CustomObject("2")
+        assertEquals(expected, actual)
+    }
+
+    @Test
     fun `test push and pop twice`() {
-        val stack = MyStack.Base<CustomObject>(maxCount = 1)
+        val stack = MyStack.LIFO<CustomObject>(maxCount = 1)
+        stack.push(item = CustomObject("1"))
+        var actual = stack.pop()
+        var expected = CustomObject("1")
+        assertEquals(expected, actual)
+        stack.push(item = CustomObject("2"))
+        actual = stack.pop()
+        expected = CustomObject("2")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test push and pop twice fifo`() {
+        val stack = MyStack.FIFO<CustomObject>(maxCount = 1)
         stack.push(item = CustomObject("1"))
         var actual = stack.pop()
         var expected = CustomObject("1")
