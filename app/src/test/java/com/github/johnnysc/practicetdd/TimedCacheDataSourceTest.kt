d2511d17f6b04fc160d0b2467694274ffd51a99e
hovhannes <johnnysc091@gmail.com>
2022-12-26 12:34:06 +0900

    added many items test

--- app/src/test/java/com/github/johnnysc/practicetdd/TimedCacheDataSourceTest.kt
+++ app/src/test/java/com/github/johnnysc/practicetdd/TimedCacheDataSourceTest.kt
@@ -28,6 +28,41 @@ class TimedCacheDataSourceTest {
         assertEquals(0, cacheDataSource.data().size)
     }
 
+    @Test
+    fun `test many items`() {
+        val now = FakeNow()
+        val cacheDataSource: CacheDataSource =
+            CacheDataSource.Timed(now, lifeTimeMillis = 1000)
+
+        cacheDataSource.add(SimpleData("one"))
+
+        now.time = 300
+        cacheDataSource.add(SimpleData("two"))
+
+        now.time = 750
+        cacheDataSource.add(SimpleData("three"))
+
+        now.time = 999
+        var expected = listOf(SimpleData("one"), SimpleData("two"), SimpleData("three"))
+        var actual = cacheDataSource.data()
+        assertEquals(expected, actual)
+
+        now.time = 1001
+        expected = listOf(SimpleData("two"), SimpleData("three"))
+        actual = cacheDataSource.data()
+        assertEquals(expected, actual)
+
+        now.time = 1301
+        expected = listOf(SimpleData("three"))
+        actual = cacheDataSource.data()
+        assertEquals(expected, actual)
+
+        now.time = 1751
+        expected = listOf()
+        actual = cacheDataSource.data()
+        assertEquals(expected, actual)
+    }
+
     private class FakeNow : Now {
 
         var time: Long = 0
