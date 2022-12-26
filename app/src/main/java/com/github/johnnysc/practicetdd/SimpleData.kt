43cec214a00dceb4c12e35e982bf19e6b1265037
hovhannes <johnnysc091@gmail.com>
2022-12-26 12:11:47 +0900

    task/005-timed-cache

---
+++ app/src/main/java/com/github/johnnysc/practicetdd/SimpleData.kt
@@ -0,0 +1,6 @@
+package com.github.johnnysc.practicetdd
+
+/**
+ * @author Asatryan on 26.12.2022
+ */
+data class SimpleData(private val value: String)
---
+++ app/src/test/java/com/github/johnnysc/practicetdd/TimedCacheDataSourceTest.kt
@@ -0,0 +1,39 @@
+package com.github.johnnysc.practicetdd
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author Asatryan on 26.12.2022
+ */
+class TimedCacheDataSourceTest {
+
+    @Test
+    fun test() {
+        val now = FakeNow()
+        val cacheDataSource: CacheDataSource =
+            CacheDataSource.Timed(now = now, lifeTimeMillis = 1000)
+
+        cacheDataSource.add(SimpleData("one"))
+
+        now.time = 990
+
+        val actual: List<SimpleData> = cacheDataSource.data()
+        val expected = SimpleData("one")
+        assertEquals(expected, actual[0])
+        assertEquals(1, actual.size)
+
+        now.time = 1010
+
+        assertEquals(0, cacheDataSource.data().size)
+    }
+
+    private class FakeNow : Now {
+
+        var time: Long = 0
+
+        override fun now(): Long {
+            return time
+        }
+    }
+}
