2d45bc5cb535cfec71048d412d462e72dc5fa2da
hovhannes <johnnysc091@gmail.com>
2023-01-16 12:03:11 +0900

    task/026-encapsulation-rule-abstract-property

--- app/src/test/java/com/github/johnnysc/practicetdd/IncapsulationRuleTest.kt
+++ app/src/test/java/com/github/johnnysc/practicetdd/IncapsulationRuleTest.kt
@@ -36,6 +36,12 @@ class IncapsulationRuleTest {
                     "\n" +
                     "    protected var page: Int = 0\n" +
                     "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(protected val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    protected abstract val page: Int\n" +
+                    "}",
         )
         textList.forEach { text ->
             val actual = rule.isValid(text = text)
@@ -77,6 +83,12 @@ class IncapsulationRuleTest {
                     "abstract class Repository(val dataSource: DataSource) {\n" +
                     "\n" +
                     "   protected var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "   protected abstract val page: Int\n" +
                     "}",
         )
         textList.forEach { text ->
